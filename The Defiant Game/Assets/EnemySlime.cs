fabff895b807ac5e4ded9d01dddd28296844715f
VictorOliveira23 <victordavidodeoliveira@gmail.com>
2021-09-25 19:29:24 +0100

    Working slime, shoots and explodes dealing damage, new particle system

--- The Defiant Game/Assets/EnemySlime.cs
+++ The Defiant Game/Assets/EnemySlime.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class EnemySlime : MonoBehaviour
 {
-    public float dieTime, damage;
+    public float dieTime, damage,missTime;
     public GameObject dieButtle;
     
     public float speed;
     private Transform player;
     private Vector2 targat;
+    private Rigidbody2D rb2D;
+    private float thrust = 1f;
+    public GameObject ballExplode;
 
 
     // Start is called before the first frame update
@@ -17,27 +20,36 @@ public class EnemySlime : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         targat = new Vector2(player.position.x, player.position.y);
+        rb2D = GetComponent<Rigidbody2D>();
         StartCoroutine(CountDownTimer());
     }
 
     void Update() {
         transform.position = Vector2.MoveTowards(transform.position, targat, speed * Time.deltaTime);
         if(transform.position.x == targat.x && transform.position.y == targat.y){
-            DestroyProjectile();
+            StartCoroutine(CountDownMiss());            
         }    
+        
     }
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(other.CompareTag("Player")){
+    private void OnTriggerStay2D(Collider2D other) {
+         if(other.CompareTag("Player")){
+            GameObject.FindObjectOfType<PlayerHealth>().UpdateHealth(-damage);
             DestroyProjectile();
         }
     }
     void DestroyProjectile(){
+        Instantiate(ballExplode, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 
     IEnumerator CountDownTimer(){
         yield return new WaitForSeconds(dieTime);
-        Destroy(gameObject);
+        DestroyProjectile();
+    }
+    IEnumerator CountDownMiss(){
+
+        yield return new WaitForSeconds(missTime);
+        DestroyProjectile();
     }
 
 }
